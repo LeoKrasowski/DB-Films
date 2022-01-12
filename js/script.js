5707a7f35cd16b313ca073e556fc4b1f533ffd2a
Leo <leokrasowski@gmail.com>
2022-01-12 21:45:24 +0100

    33

--- js/script.js
+++ js/script.js
@@ -23,7 +23,35 @@ const movieDB = {
         "Скотт Пилигрим против..."
     ]
 };
- 
+
+const btn = document.querySelector('button');
+const promoInIt = document.querySelector('.promo__interactive-list');
+
+
+btn.addEventListener('click', (event) => {
+    event.preventDefault();
+    const valueH = document.querySelector('.adding__input').value;
+    if (valueH.length > 10) {
+        movieDB.movies[movieDB.movies.length] = valueH.slice(0,15) + '...';
+    } 
+    else {
+        movieDB.movies[movieDB.movies.length] = valueH;
+    };  
+   
+    // if (movieDB.movies[movieDB.movies.length].length < valueH.length) {
+    //     movieDB.movies[movieDB.movies.length] += '...';
+    //     } 
+    movieDB.movies.sort();
+    listGenerate ();
+    document.querySelector('.adding__input').value = "";
+});
+
+const favoritfilm = document.querySelector('input[type="checkbox"]').value;
+    if (favoritfilm) {
+        console.log("Добавляем");
+    };
+
+
 movieDB.movies.sort();
 
 
@@ -40,13 +68,15 @@ movieDB.movies.sort();
  const promoBg = document.getElementsByClassName('promo__bg');
  promoBg[0].style.backgroundImage = 'url("../img/zap/bg.jpg")';
 
- const promoInIt = document.querySelector('.promo__interactive-list');
- promoInIt.innerHTML = "";
- 
- movieDB.movies.forEach((movie, i) => {
-    promoInIt.innerHTML += `
-    <li class="promo__interactive-item"> ${i+1} ${movie} 
-        <div class="delete"></div>
-    </li>`;
- });
+
+ function listGenerate () {
+    promoInIt.innerHTML = "";
+    movieDB.movies.forEach((movie, i) => {
+        promoInIt.innerHTML += `
+        <li class="promo__interactive-item"> ${i+1} ${movie} 
+            <div class="delete"></div>
+        </li>`;
+     });
+ };
+ listGenerate ();
  
