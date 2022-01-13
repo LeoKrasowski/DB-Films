783639c4ba29d6a1324ba88d664121cfef361f0d
Leo <leokrasowski@gmail.com>
2022-01-13 22:29:52 +0100

    33full

--- js/script.js
+++ js/script.js
@@ -14,69 +14,82 @@
 
 'use strict';
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
+document.addEventListener('DOMContentLoaded', () => {
 
-const btn = document.querySelector('button');
-const promoInIt = document.querySelector('.promo__interactive-list');
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
 
+    const btn = document.querySelector('button');
+    const promoInIt = document.querySelector('.promo__interactive-list');
 
-btn.addEventListener('click', (event) => {
-    event.preventDefault();
-    const valueH = document.querySelector('.adding__input').value;
-    if (valueH.length > 10) {
-        movieDB.movies[movieDB.movies.length] = valueH.slice(0,15) + '...';
-    } 
-    else {
-        movieDB.movies[movieDB.movies.length] = valueH;
-    };  
-   
-    // if (movieDB.movies[movieDB.movies.length].length < valueH.length) {
-    //     movieDB.movies[movieDB.movies.length] += '...';
-    //     } 
-    movieDB.movies.sort();
-    listGenerate ();
-    document.querySelector('.adding__input').value = "";
-});
+    btn.addEventListener('click', (event) => {
+        event.preventDefault();
+        const valueH = document.querySelector('.adding__input').value;
+        if (valueH) {
+            if (valueH.length > 10) {
+                movieDB.movies[movieDB.movies.length] = valueH.slice(0, 15) + '...';
+            }
+            else {
+                movieDB.movies[movieDB.movies.length] = valueH;
+            };
+        };
+        
 
-const favoritfilm = document.querySelector('input[type="checkbox"]').value;
+        movieDB.movies.sort();
+        listGenerate();
+        document.querySelector('.adding__input').value = "";
+    });
+
+    const favoritfilm = document.querySelector('input[type="checkbox"]').value;
     if (favoritfilm) {
         console.log("Добавляем");
     };
 
+    
 
-movieDB.movies.sort();
+    const promoAdv = document.querySelectorAll('.promo__adv img');
+    console.log(promoAdv);
+    promoAdv.forEach(element => {
+        element.remove();
+    });
+
+    const promoGenre = document.getElementsByClassName('promo__genre');
+    promoGenre[0].innerHTML = 'Драма';
+    console.log(promoGenre);
+
+    const promoBg = document.getElementsByClassName('promo__bg');
+    promoBg[0].style.backgroundImage = 'url("../img/zap/bg.jpg")';
+
+    function listGenerate() {
+        promoInIt.innerHTML = "";
+        movieDB.movies.sort();
+        movieDB.movies.forEach((movie, i) => {
+            promoInIt.innerHTML += `
+            <li class="promo__interactive-item"> ${i + 1} ${movie} 
+                <div class="delete"></div>
+            </li>`;
+        });
+        const delbtn = document.querySelectorAll('.delete');
+
+        delbtn.forEach((d, i)  => {
+            d.addEventListener('click', () => {
+                d.parentNode.remove();
+                movieDB.movies.splice(i, 1);
+                listGenerate();
+            });
+
+        });
+    };
+    listGenerate();
+});
 
 
- const promoAdv = document.querySelectorAll('.promo__adv img');
- console.log(promoAdv);
- promoAdv.forEach(element => {
-    element.remove();
- });
-
- const promoGenre = document.getElementsByClassName('promo__genre');
- promoGenre[0].innerHTML = 'Драма';
- console.log(promoGenre);
-
- const promoBg = document.getElementsByClassName('promo__bg');
- promoBg[0].style.backgroundImage = 'url("../img/zap/bg.jpg")';
 
 
- function listGenerate () {
-    promoInIt.innerHTML = "";
-    movieDB.movies.forEach((movie, i) => {
-        promoInIt.innerHTML += `
-        <li class="promo__interactive-item"> ${i+1} ${movie} 
-            <div class="delete"></div>
-        </li>`;
-     });
- };
- listGenerate ();
- 
